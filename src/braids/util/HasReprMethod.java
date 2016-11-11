780722bcb256a92868aecd243ddbe3ca558d3c16
Braids Constance <BraidsCC@users.noreply.github.com>
2016-11-11 11:16:49 -0500

    add javadoc

--- src/braids/util/HasReprMethod.java
+++ src/braids/util/HasReprMethod.java
@@ -1,5 +1,23 @@
 package braids.util;
 
+
+/**
+ * This is a simple mix-in interface that marks a class as having a
+ * __repr__ method, a la Python programming language.
+ * 
+ * __repr__ shall return a String that represents an unambiguous means  
+ * of identifying this object, e.g., for a debugger.  It may return
+ * a complete constructor call.  For example:
+ * 
+ * 
+ * @Override
+ * public String __repr__() {
+ *     return "ClassName(" + repr(field1) + "," + repr(field2) + ")";
+ * } 
+ * 
+ * 
+ * Omit the new operator.
+ */
 public interface HasReprMethod {
 	public String __repr__();
 }
